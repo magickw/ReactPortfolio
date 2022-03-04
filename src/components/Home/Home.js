af558a5f193792ca4d83b15389f7c07452f1061d
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2022-03-04 00:28:26 -0800

    updated home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,7 +13,7 @@ function Home() {
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hello! <span className="wave" role="img" aria-labelledby="wave">👋🏻</span>
+                Hello! Welcome to My Portfolio Website.
               </h1>
 
               <h1 className="heading-name">
