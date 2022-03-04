e6095e0d49fac3301d13eb093b6fccd745a6ef14
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2022-03-03 20:54:43 -0800

    updated the aboutcard

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hello, I am <span className="purple">Baofeng Guo </span>
+            from <span className="purple"> San Francisco Bay Area</span>
+            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
+            Mesra.
+            <br />
+            <br />
+            Hobbies
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ marginBlockEnd: 0, color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer"></footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
