198d4253563872897a2ae9ad32fa0a139e8c297d
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2022-03-04 19:30:12 -0800

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+//dependencies
 import React, { useState, useEffect } from "react";
-import Preloader from "./components/Pre";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+//components
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
+import Skills from "./components/Projects/Skills";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -25,14 +29,16 @@ function App() {
 
   return (
     <Router>
-      <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
         <ScrollToTop />
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/projects" component={Projects} />
           <Route path="/about" component={About} />
+          <Route path="/skills" component={Skills} />
+          <Route path="/projects" component={Projects} />
         </Switch>
         <Footer />
       </div>
