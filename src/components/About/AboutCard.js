f3d0b25c675d7611b7372f5625e0deb860c032ee
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2022-03-04 18:50:17 -0800

    added style.css

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,13 +9,11 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hello, I am <span className="blue">Baofeng Guo </span>
-            from <span className="blue"> San Francisco Bay Area</span>
+            from <span className="blue">San Francisco Bay Area</span>
             <br />I hold a Bachelor Degree in Science. I majored in Biology in college and have been working as an English - Chinese medical translator after graduation for more than 14 years. 4 years after moving to the Bay Area and inspired by its entrepreneurial atmosphere, I felt it's about time to make a change by pursuing a coding career. That's how it started. I am now currently trained at UC Berkeley Extension Coding Bootcamp for Full Stack Web Development. Do not hesitate to contact me if you need my services.
             <br />
             <br />
           </p>
-          
-
           <footer className="blockquote-footer"></footer>
         </blockquote>
       </Card.Body>
