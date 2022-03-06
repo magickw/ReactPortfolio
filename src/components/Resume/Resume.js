f9f87ba4ee846fb6aff5e92f9e8167c8ddaa4e3f
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2022-03-05 22:06:01 -0800

    updated the resum.js

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,11 +1,19 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
+import { Document, Page } from "react-pdf";
 import Particle from "../Particle";
 import cv from "../../assets/cv.pdf";
 import { FaCloudDownloadAlt } from "react-icons/fa";
 
 function Resume() {
+  const resumeLink = "https://github.com/magickw/ReactPortfolio/blob/main/src/assets/cv.pdf";
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
   return (
     <Container fluid className="resume-section">
       <Particle />
@@ -17,10 +25,12 @@ function Resume() {
           </Button>
         </Row>
         <Row className="resume">
-          
+        <Document file={resumeLink}>
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
         </Row>
         <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button variant="primary" href={pdf} target="_blank">
+          <Button variant="primary" href={cv} target="_blank">
             <FaCloudDownloadAlt />
             &nbsp;Download CV
           </Button>
