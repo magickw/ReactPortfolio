4c3025290d1a2714efb1ba77256227b341db9cea
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2022-03-07 11:25:30 -0800

    updated style.css

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import cv from "../../assets/cv.pdf";
 import { FaCloudDownloadAlt } from "react-icons/fa";
@@ -10,7 +10,7 @@ function Resume() {
   return (
     <Container fluid className="resume-section">
       <Container>
-        <Row style={{ justifyContent: "center", position: "relative" }}>
+        <Row Col={4} style={{ justifyContent: "center", position: "relative" }}>
           <Button variant="primary" href={cv} target="_blank">
             <FaCloudDownloadAlt />
             &nbsp;Download CV
